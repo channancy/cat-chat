a2229ded24f7c5393ff8d13ba020e226f7a728bb
Nancy Chan <nancychannc@gmail.com>
2017-01-19 23:27:20 -0800

    Add Firebase Auth

--- app/src/main/java/channa/com/catchat/activities/MainActivity.java
+++ app/src/main/java/channa/com/catchat/activities/MainActivity.java
@@ -1,14 +1,40 @@
 package channa.com.catchat.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 import channa.com.catchat.R;
 import channa.com.catchat.adapters.MyFragmentPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+
+    // Arbitrary request code values
+    public static final int RC_SIGN_IN = 1;
+
+    private String mUsername;
+
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +47,106 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
 
+        // Initialize Firebase components
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                // Signed in
+                if (user != null) {
+                    onSignedInInitialize(user.getDisplayName());
+                }
+                // Signed out
+                else {
+                    onSignedOutCleanup();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    // SmartLock saves user's credentials and tries to log them in
+                                    .setIsSmartLockEnabled(false)
+                                    .setProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Sign in
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            }
+            else if (resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private void onSignedInInitialize(String username) {
+        mUsername = username;
+        Log.d(TAG, "onSignedInInitialize: " + mUsername);
+
+        // Attach listener here because database rules say only authenticated users can access
+        // attachDatabaseReadListener();
+    }
+
+    private void onSignedOutCleanup() {
+        // Unset username
+        mUsername = null;
+
+        // If do not clear, see messages even though not signed in,
+        // also a bug in which see duplicate messages when signing in/out
+        // mMessageAdapter.clear();
+        // detachDatabaseReadListener();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Detach listener
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+
+        // If signed in, triggers onSignInInitialize which attaches database listener so also detach and clear adapter here
+        // Also ensures that when activity destroyed (even when nothing to do with sign out like app rotation), still cleanup
+        // detachDatabaseReadListener();
+        // mMessageAdapter.clear();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Attach listener
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                // Sign out
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
