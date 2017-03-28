8b495d9c62d04d173de6b9c2c9cc8ab56ceb8892
Nancy Chan <nancychannc@gmail.com>
2017-03-28 16:54:35 -0700

    Work on Friends tab adapter and rename files/variables

--- app/src/main/java/channa/com/catchat/adapters/FriendListAdapter.java
+++ app/src/main/java/channa/com/catchat/adapters/FriendListAdapter.java
@@ -5,8 +5,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 import channa.com.catchat.R;
+import channa.com.catchat.models.User;
 
 /**
  * Created by Nancy on 3/28/2017.
@@ -16,6 +21,7 @@ public class FriendListAdapter extends RecyclerView.Adapter<FriendListAdapter.Vi
 
     private Context mContext;
     private LayoutInflater mLayoutInflater;
+    private List<User> mFriendList;
 
     public FriendListAdapter(Context context) {
         this.mContext = context;
@@ -24,24 +30,31 @@ public class FriendListAdapter extends RecyclerView.Adapter<FriendListAdapter.Vi
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mLayoutInflater.inflate(R.layout.item_contact, parent, false);
+        View view = mLayoutInflater.inflate(R.layout.item_friend, parent, false);
         FriendListAdapter.ViewHolder holder = new FriendListAdapter.ViewHolder(view);
         return holder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        User friend = mFriendList.get(position);
+        holder.friendName.setText(friend.getName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mFriendList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView friendAvatar;
+        TextView friendName;
+
         public ViewHolder(View itemView) {
             super(itemView);
+
+            friendAvatar = (ImageView) itemView.findViewById(R.id.iv_friend_avatar);
+            friendName = (TextView) itemView.findViewById(R.id.tv_friend_name);
         }
     }
 }
