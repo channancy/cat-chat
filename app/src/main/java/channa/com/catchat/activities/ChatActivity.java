bdfbdc69a80e727964f701ce4154b79d39e87738
Nancy Chan <nancychannc@gmail.com>
2017-03-29 19:43:56 -0700

    Add ChatActivity

--- app/src/main/java/channa/com/catchat/activities/ChatActivity.java
+++ app/src/main/java/channa/com/catchat/activities/ChatActivity.java
@@ -1,7 +1,9 @@
 package channa.com.catchat.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import channa.com.catchat.R;
 
 public class ChatActivity extends AppCompatActivity {
 
