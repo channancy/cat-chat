4b9965fe52e98a566b78eca7595cdd06666d4708
Nancy Chan <nancychannc@gmail.com>
2017-03-30 16:05:21 -0700

    Adjust formatting

--- app/src/main/java/channa/com/catchat/adapters/MyFragmentPagerAdapter.java
+++ app/src/main/java/channa/com/catchat/adapters/MyFragmentPagerAdapter.java
@@ -14,6 +14,7 @@ import channa.com.catchat.fragments.FriendsTab;
  */
 
 public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
     private final static String TAG = "MyFragmentPagerAdapter";
 
     private final int mNumOfTabs = 2;
