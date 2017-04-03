1b62bd12674aab1608c94748e2efaf47b1b0eaad
Nancy Chan <nancychannc@gmail.com>
2017-04-03 16:12:10 -0700

    Add id to user model

--- app/src/main/java/channa/com/catchat/models/User.java
+++ app/src/main/java/channa/com/catchat/models/User.java
@@ -8,6 +8,7 @@ import java.util.Map;
  */
 
 public class User {
+    private String id;
     private String name;
     private String email;
     private String avatarUrl;
@@ -16,10 +17,19 @@ public class User {
 
     }
 
-    public User(String name, String email, String avatarUrl) {
+    public User(String id, String name, String email, String avatarUrl) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.avatarUrl = avatarUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -48,6 +58,7 @@ public class User {
 
     public Map<String, Object> toMap() {
         HashMap<String, Object> result = new HashMap<>();
+        result.put("id", id);
         result.put("name", name);
         result.put("email", email);
         result.put("avatarUrl", avatarUrl);
